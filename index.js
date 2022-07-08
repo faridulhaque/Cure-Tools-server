61f06a9ad4fb0632b50a27a73ee7acbc2a6f22cc
faridulhaque <faridulhaquemurshed@gmail.com>
2022-07-09 03:29:26 +0600

    few parts added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const app = express();
 
@@ -23,6 +24,8 @@ async function run() {
     await client.connect();
     const toolsCollection = client.db("cureTools").collection("tools");
     const usersCollection = client.db("cureTools").collection("users");
+    const ordersCollection = client.db("cureTools").collection("orders");
+    const reviewsCollection = client.db("cureTools").collection("reviews");
 
     // getting data for inventory section in the home page
     app.get("/tools", async (req, res) => {
@@ -46,6 +49,23 @@ async function run() {
       const result = await usersCollection.findOne(query);
       res.send(result);
     });
+    // getting logged in user's orders for my orders page
+    app.get("/myOrders", async (req, res) => {
+      const email = req.query.email;
+
+      const query = { email: email };
+      const cursor = ordersCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // getting logged in user's review for add a review page
+    app.get("/myReview/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await reviewsCollection.findOne(query);
+      res.send(result);
+    });
+
     // adding new user's data in the database
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
@@ -55,8 +75,9 @@ async function run() {
       const data = {
         $set: {
           email: user.email,
-          name: user.name,
-          img: user.image,
+
+          primaryName: user.primaryName,
+          primaryPic: user.primaryPic,
         },
       };
       const result = await usersCollection.updateOne(query, data, options);
@@ -66,18 +87,79 @@ async function run() {
     app.put("/profile/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
-      const body = req.body;
+      const user = req.body;
       const options = { upsert: true };
       const data = {
         $set: {
-          name: body.name,
-          phn: body.phn,
-          address: body.address,
-          // img: body.updatedImg,
+          profileName: user.profileName,
+          phn: user.phn,
+          address: user.address,
         },
       };
-
       const result = await usersCollection.updateOne(query, data, options);
+      res.send(result);
+    });
+    // updating payment information for my orders page in db
+    app.put("/order/payment/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const info = {
+        $set: {
+          transaction: data.transaction,
+          paymentStatus: data.paymentStatus,
+        },
+      };
+      const result = await ordersCollection.updateOne(query, info, options);
+      res.send(result);
+    });
+
+    // posting orders from inventory page
+    app.post("/orders", async (req, res) => {
+      const orders = req.body;
+      const result = await ordersCollection.insertOne(orders);
+      res.send(result);
+    });
+
+    // deleting data from my order page
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    });
+    // payment method added
+    app.post("/create-payment-intent", async (req, res) => {
+      const { totalPrice } = req.body;
+
+      const amount = parseFloat(totalPrice) * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+
+    //store reviews from the users to db
+    app.put("/review/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const data = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const info = {
+        $set: {
+          email: data.email,
+
+          ratingText: data.ratingText,
+          ratingStar: data.ratingStar,
+        },
+      };
+      
+      const result = await reviewsCollection.updateOne(query, info, options);
+
       res.send(result);
     });
   } finally {
